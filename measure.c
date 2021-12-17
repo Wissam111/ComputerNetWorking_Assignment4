0b0be78503103a18e69826edbe91593765f14ccc
Wissam111 <riki_kabha@hotmail.com>
2021-12-17 15:07:54 +0200

    final

--- measure.c
+++ measure.c
@@ -1,3 +1,4 @@
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <string.h>
@@ -10,28 +11,32 @@
 #include <stdlib.h>
 #include <time.h>
 
-char buff[256] = {0};
-int bytes_rec;
-int accp;
-socklen_t len;
-
 #define SERVER_PORT 5060
 int main(int argc, char **argv)
 {
-  int sock = socket(AF_INET, SOCK_STREAM, 0);
+
+  FILE *fptr;
+  int cubic_bytes_rec, reno_bytes_rec;
+  char cubic_buff[256], reno_buff[256];
+  int sock;
+  int cubic_accp, reno_accp;
+  int b;
+  int lis;
+  struct sockaddr_in cubic_server_addr, reno_server_addr;
+  socklen_t cubic_len, reno_len;
+  sock = socket(AF_INET, SOCK_STREAM, 0);
   if (sock == -1)
   {
     printf("could not create socket!!");
     return -1;
   }
 
-  struct sockaddr_in server_addr;
-  memset(&server_addr, 0, sizeof(server_addr));
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_port = htons(SERVER_PORT);
-  server_addr.sin_addr.s_addr = INADDR_ANY;
+  memset(&cubic_server_addr, 0, sizeof(cubic_server_addr));
+  cubic_server_addr.sin_family = AF_INET;
+  cubic_server_addr.sin_port = htons(SERVER_PORT);
+  cubic_server_addr.sin_addr.s_addr = INADDR_ANY;
 
-  int b = bind(sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
+  b = bind(sock, (struct sockaddr *)&cubic_server_addr, sizeof(cubic_server_addr));
 
   if (b == -1)
   {
@@ -39,57 +44,57 @@ int main(int argc, char **argv)
     return -1;
   }
 
-  printf("Bind() success\n");
-  int lis = listen(sock, 10);
+  lis = listen(sock, 10);
   if (lis == -1)
   {
     printf("listen FAILED , EROR!!");
     return -1;
   }
-  printf("listen() success\n");
 
-  printf("Waiting for incoming TCP-connections...\n");
+  printf("\nWaiting for incoming TCP-connections...\n");
 
-  accp = accept(sock, (struct sockaddr *)&server_addr, &len);
-  if (accp == -1)
+  cubic_accp = accept(sock, (struct sockaddr *)&cubic_server_addr, &cubic_len);
+  if (cubic_accp == -1)
   {
     printf("accept FAILED , EROR!!");
     return -1;
   }
 
-  printf("accept() success\n");
-  int count = 4114 / 2;
   clock_t start_t, end_t;
   double total_t;
   start_t = clock();
   printf("\ntime started at: %f ,  with cubic\n", (double)start_t / CLOCKS_PER_SEC);
-
   do
   {
-    bytes_rec = recv(accp, buff, 256, 0);
-    count--;
-  } while (count > 0);
+    cubic_bytes_rec = recv(cubic_accp, cubic_buff, 256, 0);
+
+  } while (cubic_bytes_rec > 0);
   end_t = clock();
+
   printf("\ntime ended at: %f ,  with cubic\n", (double)end_t / CLOCKS_PER_SEC);
   double time_first = (double)(end_t - start_t) / CLOCKS_PER_SEC;
   printf("\ntotal time cubic: %f \n", time_first);
+  close(cubic_accp);
   printf("\n----------SWITCHING ALGOS-----------\n");
+
+  reno_accp = accept(sock, (struct sockaddr *)&reno_server_addr, &reno_len);
+  if (reno_accp == -1)
+  {
+    printf("accept FAILED , EROR!!");
+    return -1;
+  }
+
   start_t = clock();
   printf("\ntime started at: %f ,  with reno\n", (double)start_t / CLOCKS_PER_SEC);
   do
   {
-    bytes_rec = recv(accp, buff, 256, 0);
+    reno_bytes_rec = recv(reno_accp, reno_buff, 256, 0);
 
-  } while (bytes_rec > 0);
+  } while (reno_bytes_rec > 0);
 
   end_t = clock();
   printf("\ntime ended at: %f ,  with reno\n", (double)end_t / CLOCKS_PER_SEC);
-
-  double time_second = (double)(end_t - start_t) / CLOCKS_PER_SEC;
-  printf("\ntotal time reno: %f \n", time_second);
-  total_t = time_first + time_second;
-
-  printf("\ntime finished at: %f ,  for both CC algos\n", total_t);
-
-  return 0;
+  time_first = (double)(end_t - start_t) / CLOCKS_PER_SEC;
+  printf("\ntotal time reno: %f \n", time_first);
+  close(sock);
 }
--- sender.c
+++ sender.c
@@ -8,14 +8,13 @@
 #include <stdio.h>
 #include <netinet/tcp.h>
 #include <stdlib.h>
-socklen_t length;
-FILE *fptr;
-char buff[256];
+
+
 char *filename = "1gb.txt";
-int bytes_sent = 0;
 #define SERVER_PORT 5060
 #define SERVER_IP_ADDRESS "192.168.56.1";
 
+//sending file
 void send_file(FILE *fp, int sock)
 {
     char data[256] = {0};
@@ -32,24 +31,26 @@ void send_file(FILE *fp, int sock)
         bzero(data, 256);
     }
 }
-
-int main(int argc, char **argv)
+// to open diff sockets
+int openingSock(int sock, char *buff, int conn, int bytes_sent, struct sockaddr_in server_addr, socklen_t len)
 {
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    FILE *fptr;
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+
     if (sock == -1)
     {
         printf("could not create socket!!");
         return -1;
     }
 
-    struct sockaddr_in server_addr;
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(SERVER_PORT);
     server_addr.sin_addr.s_addr = INADDR_ANY;
 
-    int connc = connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
-    if (connc == -1)
+    conn = connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    if (conn == -1)
     {
         printf("could not make Connection");
         return -1;
@@ -61,43 +62,37 @@ int main(int argc, char **argv)
         printf("could not read file");
         exit(1);
     }
-    length = sizeof(buff);
+    len = sizeof(buff);
 
-    //cubic
-    if (getsockopt(sock, IPPROTO_IP, TCP_CONGESTION, buff, &length) != 0)
-    {
-        perror("getsockopt");
-        return -1;
-    }
-    // puts(buff);
-    printf("Current: %s\n", buff);
     for (int i = 0; i < 5; i++)
     {
         send_file(fptr, sock);
     }
-    printf("-------- sending with cubic done------- \n");
-    //reno
     strcpy(buff, "reno");
-    length = strlen(buff);
-    if (setsockopt(sock, IPPROTO_IP, TCP_CONGESTION, buff, length) != 0)
+    len = strlen(buff);
+//switching CC algo to reno
+    if (setsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buff, len) != 0)
     {
         perror("setsockopt");
         return -1;
     }
-    length = sizeof(buff);
-    if (getsockopt(sock, IPPROTO_IP, TCP_CONGESTION, buff, &length) != 0)
-    {
-        perror("getsockopt");
-        return -1;
-    }
-    printf("New: %s\n", buff);
-    //send file
-    for (int i = 0; i < 5; i++)
-    {
-        // bytes_sent += send(sock, fgets(buff, 256, fptr), 256, 0);
-        send_file(fptr, sock);
-    }
-    printf("-------- sending with reno done------- \n");
+
     close(sock);
-    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    FILE *fptr;
+    socklen_t len;
+    int cubic_bytes_sent = 0, reno_bytes_sent = 0;
+    // char cubic_buff[256], reno_buff[256];
+    char buff[256];
+    int cubic_sock, reno_sock;
+    int cubic_conn, reno_conn;
+    struct sockaddr_in cubic_server_addr, reno_server_addr;
+    len = sizeof(buff);
+    printf("Current: cubic\n");
+    openingSock(cubic_sock, buff, cubic_conn, cubic_bytes_sent, cubic_server_addr, len);
+    openingSock(reno_sock, buff, reno_conn, reno_bytes_sent, reno_server_addr, len);
+    printf("New: %s\n", buff);
 }
